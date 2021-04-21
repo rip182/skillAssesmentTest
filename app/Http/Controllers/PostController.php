a74f94dfd5c72716850880e6a9326cc25e88c207
rip182 <ghostproject50@gmail.com>
2021-04-21 19:47:34 +0800

    env set for homestead and api CRUD

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return PostResource::collection(Post::paginate());
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+        'title' => 'required',
+        'body' => 'required',
+        'user_id' => 'required|integer'
+        ]);
+         
+        return  new PostResource(Post::create($request->all())); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @param  string $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post, string $id)
+    {
+        return new PostResource($post::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string $id
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post, string $id)
+    {
+        // dd($request->all());
+        $post = $post::findOrFail($id);
+        $post->update($request->all());
+
+        return new PostResource($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @param  string $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post, string $id)
+    {
+        return $post::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Public Routes
+Route::get('/posts',[PostController::class,'index']);
+Route::get('/posts/{id}',[PostController::class,'show']);
+Route::post('/posts',[PostController::class,'store']);
+Route::put('/posts/{id}',[PostController::class,'update']);
+Route::delete('/posts/{id}', [PostController::class,'destroy']);
+
+
+//Protected routes 
+
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
