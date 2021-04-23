500661fc3d7e20c177884495db19153748d8d4e0
rip182 <ghostproject50@gmail.com>
2021-04-23 14:29:57 +0800

    mockdata for post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create();
+        foreach (range(1,20) as $index) {
+            DB::table('posts')->insert([
+                'title' => $faker->word,
+                'body' => $faker->sentence,
+                'user_id' => 2,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ]);
+        }
     }
 }
